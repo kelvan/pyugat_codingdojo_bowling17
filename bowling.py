61b33e668faece38ae31b998365b0a181678c2e0
Florian Schweikert <kelvan@ist-total.org>
2017-10-24 20:41:43 +0200

    first working version

---
+++ bowling.py
@@ -0,0 +1,50 @@
+def split_frames(inp):
+    """
+    split input data to frames
+    :param inp: str of space separated frames
+    :return: list of frames
+    """
+    return inp.split(' ')
+
+
+def frame_score(frame):
+    """
+    Calculate frame score without look ahead score
+    :param frame: str
+    :return: (score_of_frame, look_ahead)
+    """
+    if frame.endswith('/'):
+        return (10, 1)
+
+    strikes = frame.count('X')
+    return (
+        sum([int(throw) for throw in frame.strip('-').strip('X')]) + strikes*10,
+        2 if strikes else 0
+    )
+
+
+def get_next_throws(frames, i, steps):
+    """
+    get next throws as frame of size steps
+    :param frames:
+    :param i: index of reference frame
+    :param steps: how many steps to take
+    :return: next throws as str
+    """
+    return ''.join(frames[i+1:])[:steps]
+
+
+def score(game):
+    """
+    Calculate game score
+    :param game: str containing frames seperated by spaces
+    :return: total game score
+    """
+    frames = split_frames(game)
+
+    game_score = 0
+
+    for i in range(10):
+        score, steps = frame_score(frames[i])
+        game_score += score + frame_score(get_next_throws(frames, i, steps))[0]
+    return game_score
---
+++ tests.py
@@ -0,0 +1,57 @@
+from bowling import score, split_frames, frame_score, get_next_throws
+
+
+def test_split_frame():
+    assert split_frames('5/ 5/ X X X') == ['5/', '5/', 'X', 'X', 'X']
+
+
+def test_frame_only_score_strike():
+    assert frame_score('X') == (10, 2)
+
+
+def test_frame_only_score_spare():
+    assert frame_score('5/') == (10, 1)
+
+
+def test_frame_only_score_miss():
+    assert frame_score('9-') == (9, 0)
+
+
+def test_frame_only_score():
+    assert frame_score('53') == (8, 0)
+
+
+def test_get_next_throws_strike_spare():
+    assert get_next_throws(['5/', '5/', 'X', '5/', 'X'], 2, 2) == '5/'
+
+
+def test_get_next_throws_spare_strike():
+    assert get_next_throws(['5/', '5/', 'X', '5/', 'X'], 1, 1) == 'X'
+
+
+def test_get_next_throws_empty():
+    assert get_next_throws(['5/', '5/', 'X', '5/', 'X'], 1, 0) == ''
+
+
+def test_all_strikes():
+    assert score('X X X X X X X X X X X X') == 300
+
+
+def test_all_spares():
+    assert score('5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5') == 150
+
+
+def test_all_nine_misses():
+    assert score('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-') == 90
+
+
+def test_all_misses():
+    assert score('-- -- -- -- -- -- -- -- -- --') == 0
+
+
+def test_all_misses():
+    assert score('-- -- -- -- -/ 11 -- -- -- --') == 13
+
+
+def test_game_score_two_strikes():
+    assert score('9- 9- 9- X X 54 9- 9- 9- 9-') == 116
